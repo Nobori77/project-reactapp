a6daa2d3c6f78df7ef32c34e8e0867705d411b37
hoo2e2-glitch <hoo.2e2@gmail.com>
2026-04-27 17:34:19 +0900

    feat: backup

--- src/pages/eum/study/attendance/StudyAttendanceComponent.jsx
+++ src/pages/eum/study/attendance/StudyAttendanceComponent.jsx
@@ -1,12 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
+import StudyAttendancePopup from './StudyAttendancePopup';
 
+// 출석체크 화면
 const StudyAttendanceComponent = () => {
-    return (
+
+    return(
+        <>
         <div>
-            출석체크 완료
+        출석체크 매일 출석하고 보상을 받아요  ✅오늘 출석 완료! +30 XP 획득했어요!
         </div>
+        <div>
+        🔥 7일 현재 연속 출석 📅 14일 이번달 출석 ⚡1,240 총 획득 EXP 🏅3개 획득 뱃지
+        </div>
+        <div>
+        달력
+        </div>
+        <div>
+        이번주
+        </div>
+        <div>
+        출석보상
+        </div>
+        
+        </>
     );
+
 };
 
 export default StudyAttendanceComponent;
---
+++ src/pages/eum/study/style.js
@@ -0,0 +1,23 @@
+import styled from "styled-components";
+import { Link } from "react-router-dom";
+
+
+export const HeroBox = styled.section`
+  /* background: #ff7a00;
+  color: white; */
+  border-radius: 16px;
+  padding: 28px 22px;
+  margin-bottom: 18px;
+
+  .banner {
+    font-size: 24px;
+    font-weight: 800;
+    margin: 0 0 8px;
+  }
+
+  .category {
+    font-size: 15px;
+    line-height: 1.5;
+    margin: 5 0 18px;
+  }
+`;
