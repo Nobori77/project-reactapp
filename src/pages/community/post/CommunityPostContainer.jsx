318e6c6f04c634bd2247966b2b51143797bdb600
scminy0527-rgb <scminy0527@gmail.com>
2026-06-06 16:05:46 +0900

    refactor: 채팅방 생성 버튼 재사용성 강화

--- src/pages/community/post/CommunityPostContainer.jsx
+++ src/pages/community/post/CommunityPostContainer.jsx
@@ -1,46 +1,25 @@
-import React, { useState } from "react";
-import { ColumnBlock, ActionBtn } from "../communityStyle";
+import React from "react";
+import { ColumnBlock } from "../communityStyle";
 import T from "../communityTextStyle";
 import PostListSection from "./postComponents/PostListSection.jsx";
 import LiveChatListSection from "./postComponents/LiveChatListSection.jsx";
-import { useChatContext } from "../context/ChatContext";
 import { HeaderBlock } from "./communityPostContainerStyle";
-import useAuthStore from "../../../store/authStore";
-import LoginRequiredPopup from "../common/LoginRequiredPopup";
+import CreateChatRoomButton from "../chat/chatComponents/CreateChatRoomButton";
 
 const S = {
   ColumnBlock,
-  ActionBtn,
   HeaderBlock,
 };
 
 // 커뮤니티 메인 (일부 채팅방, 게시글 리스트)
 const CommunityPostContainer = () => {
-  const { openCreateChatRoom } = useChatContext();
-  const { isAuthenticated } = useAuthStore();
-  const [showLoginPopup, setShowLoginPopup] = useState(false);
-
-  const handleCreateChatRoom = () => {
-    if (!isAuthenticated) {
-      setShowLoginPopup(true);
-    } else {
-      openCreateChatRoom();
-    }
-  };
-
   console.log("메인 영역 그려지기");
   return (
     <div>
-      <LoginRequiredPopup
-        isOpen={showLoginPopup}
-        onClose={() => setShowLoginPopup(false)}
-      />
       <S.ColumnBlock>
         <S.HeaderBlock>
           <T.H6Bold>실시간 채팅방</T.H6Bold>
-          <S.ActionBtn $type="submit" onClick={handleCreateChatRoom}>
-            +채팅방 만들기
-          </S.ActionBtn>
+          <CreateChatRoomButton />
         </S.HeaderBlock>
         {/* 채팅방 리스트 목록 */}
         <LiveChatListSection />
